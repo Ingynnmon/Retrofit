0f299a736aef5096d155e07d82d0d909936f656c
ingynnmon <ingynnmon@ucsy.edu.mm>
2019-05-16 16:01:42 +0630

    modified post,put,patch,delete requests

--- app/src/main/java/com/example/retrofit/JsonPlaceHolderApi.java
+++ app/src/main/java/com/example/retrofit/JsonPlaceHolderApi.java
@@ -11,6 +11,16 @@ import retrofit2.http.Path;
 import retrofit2.http.Query;
 import retrofit2.http.QueryMap;
 import retrofit2.http.Url;
+import retrofit2.http.POST;
+
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FieldMap;
+import retrofit2.http.FormUrlEncoded;
+
+import retrofit2.http.DELETE;
+import retrofit2.http.PATCH;
+import retrofit2.http.PUT;
 
 public interface JsonPlaceHolderApi {
 
@@ -19,6 +29,56 @@ public interface JsonPlaceHolderApi {
     Call<List<Post>> getPosts();*/
 
     //Url manipulation
+    /*@GET("posts")
+    Call<List<Post>> getPosts(
+            @Query("userId") Integer[] userId,
+            @Query("_sort") String sort,
+            @Query("_order") String order
+    );
+
+    @GET("posts")
+    Call<List<Post>> getPosts(@QueryMap Map<String, String> parameters);
+
+    @GET("posts/{id}/comments")
+    Call<List<Comment>> getComments(@Path("id") int postId);
+
+    @GET
+    Call<List<Comment>> getComments(@Url String url);
+}*/
+
+    //////Post
+  /*  @GET("posts")
+    Call<List<Post>> getPosts(
+            @Query("userId") Integer[] userId,
+            @Query("_sort") String sort,
+            @Query("_order") String order
+    );
+
+    @GET("posts")
+    Call<List<Post>> getPosts(@QueryMap Map<String, String> parameters);
+
+    @GET("posts/{id}/comments")
+    Call<List<Comment>> getComments(@Path("id") int postId);
+
+    @GET
+    Call<List<Comment>> getComments(@Url String url);
+
+    @POST("posts")
+    Call<Post> createPost(@Body Post post);
+
+    @FormUrlEncoded
+    @POST("posts")
+    Call<Post> createPost(
+            @Field("userId") int userId,
+            @Field("title") String title,
+            @Field("body") String text
+    );
+
+    @FormUrlEncoded
+    @POST("posts")
+    Call<Post> createPost(@FieldMap Map<String, String> fields);
+}*/
+
     @GET("posts")
     Call<List<Post>> getPosts(
             @Query("userId") Integer[] userId,
@@ -34,4 +94,28 @@ public interface JsonPlaceHolderApi {
 
     @GET
     Call<List<Comment>> getComments(@Url String url);
+
+    @POST("posts")
+    Call<Post> createPost(@Body Post post);
+
+    @FormUrlEncoded
+    @POST("posts")
+    Call<Post> createPost(
+            @Field("userId") int userId,
+            @Field("title") String title,
+            @Field("body") String text
+    );
+
+    @FormUrlEncoded
+    @POST("posts")
+    Call<Post> createPost(@FieldMap Map<String, String> fields);
+
+    @PUT("posts/{id}")
+    Call<Post> putPost(@Path("id") int id, @Body Post post);
+
+    @PATCH("posts/{id}")
+    Call<Post> patchPost(@Path("id") int id, @Body Post post);
+
+    @DELETE("posts/{id}")
+    Call<Void> deletePost(@Path("id") int id);
 }
